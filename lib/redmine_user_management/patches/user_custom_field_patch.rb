d3f7f08b90c208c52dbc173207df07a2c2a0efce
Frederico Camara <frederico.camara@serpro.gov.br>
2022-09-30 15:44:23 -0300

    Fix: Class Patches

---
+++ lib/redmine_user_management/patches/user_custom_field_patch.rb
@@ -0,0 +1,32 @@
+# Redmine User Managerment - A Redmine Plugin
+# Copyright (C) 2021  Frederico Camara
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+module RedmineUserManagement
+  module Patches
+    module UserCustomFieldPatch
+      def self.included(base)
+        base.class_eval do
+          has_many :user_manager_ucfs, :dependent => :delete_all
+        end
+      end
+    end
+  end
+end
+
+unless UserCustomField.included_modules.include?(RedmineUserManagement::Patches::UserCustomFieldPatch)
+  UserCustomField.send(:include, RedmineUserManagement::Patches::UserCustomFieldPatch)
+end
